a4011127bd3ea8659c01cd62d84ff917e5d01dc5
molong <ycgpp@126.com>
2016-09-17 18:16:08 +0800

    编辑器增加源代码显示

--- public/plugs/editor/js/simditor.js
+++ public/plugs/editor/js/simditor.js
@@ -1894,7 +1894,7 @@ Toolbar = (function(superClass) {
       return;
     }
     if (!$.isArray(this.opts.toolbar)) {
-      this.opts.toolbar = ['bold', 'italic', 'underline', 'strikethrough', '|', 'ol', 'ul', 'blockquote', 'code', '|', 'link', 'image', '|', 'indent', 'outdent'];
+      this.opts.toolbar = ['html', '|', 'bold', 'italic', 'underline', 'strikethrough', '|', 'ol', 'ul', 'blockquote', 'code', '|', 'link', 'image', '|', 'indent', 'outdent'];
     }
     this._render();
     this.list.on('click', function(e) {
