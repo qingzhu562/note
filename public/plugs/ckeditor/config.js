0867a63089d601af9cd9af58acae7b3dc7921a5c
molong <ycgpp@126.com>
2016-12-10 15:29:01 +0800

    编辑器配置

--- public/plugs/ckeditor/config.js
+++ public/plugs/ckeditor/config.js
@@ -27,7 +27,7 @@ CKEDITOR.editorConfig = function( config ) {
 	//config.contentsCss = "ckeditor/contents.css"
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript,Iframe,Print,Save';
+	config.removeButtons = 'Subscript,Superscript,Iframe,Print,Save';
 
 	// Set the most common block elements.
 	//config.format_tags = 'p;h1;h2;h3;pre';
